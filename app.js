65685c5b608c66893b64aea283c496e8585a28ff
Haizyfox <bradbarrow@gmail.com>
2014-04-29 21:09:17 +1000

    Implement the form

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var app = angular.module("dribbbleScorer", []);
 
 app.controller("DribbbleController", function($scope){
+  $scope.newPlayer = null; // Our model value is null by default
   $scope.players = ["Tom", "Dick", "Harry"];
+
+  // Adds a player to the list of players
+  $scope.addPlayer = function(player){
+    $scope.players.push(player);
+  }
+
 });
 
