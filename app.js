6c4771242b67a1e4089508361aa5e5ab8a1bf5bc
Haizyfox <bradbarrow@gmail.com>
2014-04-30 09:52:37 +1000

    Implement DribbblePlayer Factory

--- app.js
+++ app.js
@@ -28,3 +28,30 @@ app.controller("DribbbleController", function($scope, $http){
   }
 });
 
+// We need to inject the $http service in to our factory
+app.factory("DribbblePlayer",function($http){
+  // Define the DribbblePlayer function
+  var DribbblePlayer = function(player){
+    // Define the initialize function
+    this.initialize = function () {
+      // Fetch the player from Dribbble
+      var url = 'http://api.dribbble.com/players/' + player + '?callback=JSON_CALLBACK';
+      var playerData = $http.jsonp(url);
+      var self = this;
+
+      // When our $http promise resolves
+      // Use angular.extend to extend 'this'
+      // with the properties of the response
+      playerData.then(function(response){
+        angular.extend(self, response.data);
+      });
+    }
+
+    // Call the initialize function for every new instance
+    this.initialize();
+  }
+
+  // Return a reference to the function
+  return (DribbblePlayer);
+});
+
